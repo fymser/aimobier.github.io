c7b07a2a730b77620e012e22d7e6d5daec25fdd4
荆文征 <200739491@qq.com>
2018-03-28 18:18:01 +0800

    完成 修改历史功能
    1. 增加 timeago.min.js 完成 两个时间间隔的描述
    2. github 请求

--- themes/jingwen/source/assets/js/githubhistory.js
+++ themes/jingwen/source/assets/js/githubhistory.js
@@ -1,33 +1,88 @@
 const GitHubHistoryReq = {
-  requestBodyParams:{
+  requestBodyParams: {
     sha: "make-blog", // 分支名称
     path: "source/_posts/", // 文章存储路径
-    paramF:function(fn){ // 返回请求的参数对象
+    paramF: function(fn) { // 返回请求的参数对象
       return {
-          sha: "make-blog",
-          path: "source/_posts/"+fn,
+        sha: "make-blog",
+        path: "source/_posts/" + fn,
       }
     }
   },
-  requestUrlParams:{
+  requestUrlParams: {
     user: "aimobier", // 用户名称
     repos: "aimobier.github.io", // 仓库名称
-    urlF: function(){ // 返回完整的请求链接地址
-      return "https://api.github.com/repos/"+this.user+"/"+this.repos+"/commits";
+    urlF: function() { // 返回完整的请求链接地址
+      return "https://api.github.com/repos/" + this.user + "/" + this.repos + "/commits";
     }
   },
-  Request:function(){
+  Request: function() {
     const fileName = $("#CurrentFileName").text();
     if (fileName !== null || fileName !== undefined || fileName !== '') {
-      $.getJSON(this.requestUrlParams.urlF(),this.requestBodyParams.paramF(fileName),this.requestSuccess);
+      $.getJSON(this.requestUrlParams.urlF(), this.requestBodyParams.paramF(fileName), this.requestSuccess.bind(this));
     };
   },
-  requestSuccess:function(body){
+  timeSince: function(date) {
 
-      console.log(body);
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+      return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+  },
+  returnHtmlElment: function(commit) {
+
+    const date = new Date(commit.commit.committer.date);
+
+    commit.commit.message = commit.commit.message.replace(/\n/g,"<br>");
+
+    console.log(commit.commit.message);
+
+    return '\
+    <li class="media g-brd-around g-brd-gray-light-v4 g-pa-20 g-mb-minus-1">\
+      <div class="d-flex g-mt-2 g-mr-15">\
+        <a target="_blank" href="' + commit.author.html_url + '"><img class="g-width-30 g-height-30 rounded-circle" src="' + commit.author.avatar_url + '" alt="Image Description"></a>\
+      </div>\
+      <div class="media-body">\
+        <div class="d-flex justify-content-between">\
+          <a target="_blank" href="' + commit.author.html_url + '"><strong class="g-font-size-9">' + commit.author.login + '</strong></a>\
+          <a target="_blank" href="' + commit.html_url + '"><span class="align-self-center g-font-size-9 text-nowrap">' + (commit.sha.slice(0, 5) + '..') + '</span></a>\
+        </div>\
+        <span class="align-self-center g-font-size-9 text-nowrap g-color-gray-dark-v4">'+timeago(null, 'zh_CN').format(date)+'</span>\
+        <span class="d-block g-font-size-11">' + commit.commit.message + '</span>\
+      </div>\
+    </li>\
+    ';
+  },
+  requestSuccess: function(body) {
+
+    var liRes = body.map(function(item, index, input) {
+      return this.returnHtmlElment(item);
+    }.bind(this));
+
+    $("#commit-history").html(liRes.join(""));
   }
 }
 
-$(function(){
-    GitHubHistoryReq.Request();
+$(function() {
+  GitHubHistoryReq.Request();
 });
